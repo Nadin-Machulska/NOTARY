5a062fd09ca84ceb2c713340258f45796af22de0
Nadin-Machulska <rumbachka60@gmail.com>
2022-12-21 20:25:32 +0200

    add second lightbox

--- script/lightbox.js
+++ script/lightbox.js
@@ -43,3 +43,50 @@ function showSlide(n) {
   slides[slideIndex - 1].style.display = 'block';
   modalPreviews[slideIndex - 1].className += ' active';
 };
+
+
+let slideIndexSecond = 1;
+showSlidesSecond(slideIndexSecond);
+
+
+function openLightboxSecond() {
+  document.getElementById('Lightbox-second').style.display = 'block';
+}
+
+function closeLightboxSecond() {
+  document.getElementById('Lightbox-second').style.display = 'none';
+};
+
+
+function changeSlideSecond(n) {
+  showSlidesSecond(slideIndexSecond += n);
+};
+
+function toSlideSecond(n) {
+  showSlidesSecond(slideIndexSecond = n);
+};
+
+
+function showSlidesSecond(n) {
+  const slides = document.getElementsByClassName('slide-second');
+  let modalPreviewSecond = document.getElementsByClassName('modal-preview-second');
+
+  if (n > slides.length) {
+    slideIndexSecond = 1;	
+  };
+  
+  if (n < 1) {
+    slideIndexSecond = slides.length;
+  };
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  };
+  
+  for (let i = 0; i < modalPreviewSecond.length; i++) {
+    modalPreviewSecond[i].className = modalPreviewSecond[i].className.replace(' active', '');
+  };
+  
+  slides[slideIndexSecond - 1].style.display = 'block';
+  modalPreviewSecond[slideIndexSecond - 1].className += ' active';
+};
